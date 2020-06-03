22a3d4cbe0e72625cdaa46986922638409625df0
Emily Weyda <54867682+weydaej@users.noreply.github.com>
2020-06-02 21:39:28 -0400

    learning js

---
+++ njtest/learning-js.js
@@ -0,0 +1,19 @@
+// javascript
+var hello = "hello, world!";
+console.log(hello);
+
+console.log("this is my first \"%s\" program written in %s", hello, "JavaScript")
+
+var nums = {
+    twenty: 20,
+    thirty: 30,
+    forty: 40
+};
+console.log("%O is my nums object ", nums);
+console.log(nums);
+console.log(nums.forty);
+
+var arr = ["hi", "hello", "hey", "howdy", "hewro"];
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
