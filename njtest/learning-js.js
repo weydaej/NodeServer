5b846dd5f4ab69e129dbf7b43eb79d640ddfe5d7
Emily Weyda <54867682+weydaej@users.noreply.github.com>
2020-07-13 14:36:49 -0400

    added fib function

--- njtest/learning-js.js
+++ njtest/learning-js.js
@@ -239,3 +239,17 @@ myUser.setBirthday("11/17/1996")
 myUser.setUsername("why_duh")
 myUser.setPassword("thisisasecret")
 cl(myUser)
+
+function fib(n) {
+    if (n == 0) {
+        return 0;
+    } else if (n == 1) {
+        return 1;
+    } else {
+        return fib(n - 1) + fib(n - 2)
+    }
+}
+
+for (let i = 0; i < 20; i++) {
+    cl(fib(i))
+}
